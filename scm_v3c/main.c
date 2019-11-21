a140e52ae70bc2e12c68378448f2faecdd9003ef
unknown <brian.kilberg@gmail.com>
2019-11-20 18:25:55 -0800

    reduced IMU tx

--- scm_v3c/main.c
+++ scm_v3c/main.c
@@ -240,7 +240,7 @@ int main(void) {
 		imu_measurement.gyro_z.value = 1111;
 		
 		//send measurement
-		for( i = 0; i < 1000; i++){
+		for( i = 0; i < 10000; i++){
 		}
 		send_imu_packet(imu_measurement);
 		
