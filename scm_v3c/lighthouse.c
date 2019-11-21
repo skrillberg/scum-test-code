8e754b07f7a6d822556707f82d52ffd8dc37dc58
unknown <brian.kilberg@gmail.com>
2019-11-20 19:00:02 -0800

    tweaking gpo

--- scm_v3c/lighthouse.c
+++ scm_v3c/lighthouse.c
@@ -72,7 +72,7 @@ void initialize_mote_lighthouse(){
 	//GPO_enables(0xFFFF);
 	
 	//Set GPIOs except interrupts as outputs
-	GPO_enables(0x00FF);
+	GPO_enables(0x000F);
 
 	// Set HCLK source as HF_CLOCK
 	set_asc_bit(1147);
