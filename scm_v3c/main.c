2175c4e3d4ef04ec43b5e58fb2774e44341ee77e
unknown <brian.kilberg@gmail.com>
2019-11-20 16:11:38 -0800

    gpio toggle in while loop

--- scm_v3c/main.c
+++ scm_v3c/main.c
@@ -229,7 +229,7 @@ int main(void) {
 		imu_measurement.gyro_z.value = 1111;
 		
 		//send measurement
-		for( i = 0; i < 10000; i++){
+		for( i = 0; i < 1000; i++){
 		}
 		send_imu_packet(imu_measurement);
 		
