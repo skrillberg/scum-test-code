72109da99d07a8408867bf9aab58fdda6a9c2ed2
unknown <brian.kilberg@gmail.com>
2020-02-05 13:45:19 -0800

    tuned

--- scm_v3c/lighthouse.c
+++ scm_v3c/lighthouse.c
@@ -13,8 +13,8 @@
 #include "Memory_map.h"
 #include <stdbool.h>
 
-#define QX3_FINE 16
-#define QX3_MID 20
+#define QX3_FINE 26
+#define QX3_MID 26
 #define HCLOCK_ERROR 924/1000
 extern char send_packet[127];
 
