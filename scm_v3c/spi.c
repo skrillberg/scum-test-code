756499a28c2345994c6390a028bf21fc1e2dde11
unknown <brian.kilberg@gmail.com>
2020-02-12 15:23:35 -0800

    fixing imu life

--- scm_v3c/spi.c
+++ scm_v3c/spi.c
@@ -120,7 +120,7 @@ void test_imu_life() {
 	
 		imu_measurement.acc_x.value = 66;
 		//send_imu_packet(imu_measurement);
-	//read_byte = read_imu_register(write_byte);	
+	read_byte = read_imu_register(write_byte);	
 	//imu_measurement.acc_x.value = read_byte;
 	//send_imu_packet(imu_measurement);
 	
