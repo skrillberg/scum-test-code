2b271412bdc6a82975eece91c8376e375b84d821
unknown <brian.kilberg@gmail.com>
2020-02-27 16:47:05 -0800

    20000 delay

--- scm_v3c/main.c
+++ scm_v3c/main.c
@@ -290,7 +290,7 @@ int main(void) {
 		imu_measurement.gyro_z.value = read_gyro_z();
 		
 		//send measurement
-		for( i = 0; i < 40000; i++){
+		for( i = 0; i < 20000; i++){
 		}
 		send_imu_packet(imu_measurement);
 		
