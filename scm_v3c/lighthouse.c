8bffb0900c4c6f1ded18d891902fdada74110d0c
unknown <brian.kilberg@gmail.com>
2020-03-04 13:20:48 -0800

    trying full state machine for lighthouse

--- scm_v3c/lighthouse.c
+++ scm_v3c/lighthouse.c
@@ -944,10 +944,10 @@ void lh_int_cb(int level){
 			
 
 			
-				//pulse_width = (uint32_t)((timestamp_fall - timestamp_rise)*HCLOCK_ERROR);
+		pulse_width = (uint32_t)((timestamp_fall - timestamp_rise)*HCLOCK_ERROR);
 			// Identify what kind of pulse this was
 			//pulse_type = 5;
-		pulse_width = 1330;
+		//pulse_width = 1330;
 	
     
     val = pulse_width < 585  && pulse_width > 100 ;
